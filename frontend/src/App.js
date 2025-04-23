fdfc8a53076956c8bca1bb7dd59e1f04eb2296f9
hugosmrgkr <hugosimorangkir10@gmail.com>
2025-04-23 09:22:40 +0700

    buat API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,28 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('Memuat pesan...');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/greeting')
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => {
+        console.error('Gagal fetch:', err);
+        setMessage('Gagal mengambil data dari Laravel API.');
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>Pesan dari Laravel API:</strong>
         </p>
+        <p>{message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +35,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
